de96e0d58037fab1b94ff0df0229d76d8ae5d574
Nadav Rotem <nadav256@gmail.com>
2024-11-24 14:06:41 -0800

    Add a script to dump weights.

---
+++ scripts/dump_weights.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import sys, os
+import torch
+
+device = torch.device('cpu')
+
+if len(sys.argv) != 2:
+    print("Usage: python load_model.py <path to model>")
+    sys.exit(1)
+
+if os.path.exists(sys.argv[1]) == False:
+    print("File does not exist")
+    sys.exit(1)
+
+model = torch.load(sys.argv[1], weights_only=True, map_location=device)
+
+def make_safe_name(name):
+    for i in range(len(name)):
+        if name[i] not in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_.":
+            name[i] = "_"
+
+#iterate over the tensors in the model and save them to a file.
+for tensor_name in model:
+    tensor = model[tensor_name]
+    numpy_array = tensor.cpu().numpy()
+    bytes = numpy_array.tobytes()
+    # remove invalid characters from the tensor name
+    make_safe_name(tensor_name)
+    #save the contents of the tensor to a file
+    with open(sys.argv[1]+"."+tensor_name + ".bin", "wb") as f:
+        f.write(bytes)
