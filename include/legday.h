d536409384d4492ef66972a542c505cec60309b6
Nadav Rotem <nadav256@gmail.com>
2024-11-24 16:40:00 -0800

    format

--- include/legday.h
+++ include/legday.h
@@ -1,19 +1,18 @@
 #ifndef LIB_LEGDAY_H
 #define LIB_LEGDAY_H
 
-#include <vector>
-#include <span>
 #include <cstdint>
+#include <span>
+#include <vector>
 
-namespace legday
-{
-  /// Convert a buffer to a transposed buffer. Each bit is extracted and placed
-  /// consecutively. The size of the returned buffer is equal to the size of the
-  // input buffer. The input buffer must be a multiple of 8.
-  std::vector<uint8_t> transpose(std::span<uint8_t> buffer);
+namespace legday {
+/// Convert a buffer to a transposed buffer. Each bit is extracted and placed
+/// consecutively. The size of the returned buffer is equal to the size of the
+// input buffer. The input buffer must be a multiple of 8.
+std::vector<uint8_t> transpose(std::span<uint8_t> buffer);
 
-  /// Reverses the transpose operation.
-  std::vector<uint8_t> untranspose(std::span<uint8_t> in);
-}
+/// Reverses the transpose operation.
+std::vector<uint8_t> untranspose(std::span<uint8_t> in);
+} // namespace legday
 
 #endif // LIB_LEGDAY_H
--- lib/liblegday.cpp
+++ lib/liblegday.cpp
@@ -3,27 +3,23 @@
 
 #include <cassert>
 
-static constexpr uint8_t nthbit(uint8_t byte, size_t n)
-{
+static constexpr uint8_t nthbit(uint8_t byte, size_t n) {
   return (byte >> n) & 1;
 }
 
-std::vector<uint8_t> legday::transpose(std::span<uint8_t> buffer)
-{
+std::vector<uint8_t> legday::transpose(std::span<uint8_t> buffer) {
   assert(buffer.size() % 8 == 0);
   std::vector<uint8_t> result(buffer.size(), 0);
   // The transposed buffer is split into 8 parts.
   size_t num_parts = buffer.size() / 8;
 
   // For each destination byte in a part of the transposed buffer.
-  for (size_t dest = 0; dest < num_parts; dest++)
-  {
+  for (size_t dest = 0; dest < num_parts; dest++) {
     // The source offset is multiplied by 8 because we pack 8 bytes at once.
     size_t src = dest * 8;
 
     // For each bit in the source bytes.
-    for (int bit = 0; bit < 8; bit++)
-    {
+    for (int bit = 0; bit < 8; bit++) {
       result[dest + bit * num_parts] |= nthbit(buffer[src + 0], bit) << 0 |
                                         nthbit(buffer[src + 1], bit) << 1 |
                                         nthbit(buffer[src + 2], bit) << 2 |
@@ -37,8 +33,7 @@ std::vector<uint8_t> legday::transpose(std::span<uint8_t> buffer)
   return result;
 }
 
-std::vector<uint8_t> legday::untranspose(std::span<uint8_t> in)
-{
+std::vector<uint8_t> legday::untranspose(std::span<uint8_t> in) {
   assert(in.size() % 8 == 0);
   std::vector<uint8_t> result(in.size(), 0);
 
@@ -46,11 +41,9 @@ std::vector<uint8_t> legday::untranspose(std::span<uint8_t> in)
   size_t num_parts = in.size() / 8;
 
   // For each destination byte in a part of the transposed buffer.
-  for (size_t dest = 0; dest < num_parts; dest++)
-  {
+  for (size_t dest = 0; dest < num_parts; dest++) {
     // For each bit:
-    for (int bit = 0; bit < 8; bit++)
-    {
+    for (int bit = 0; bit < 8; bit++) {
       result[dest * 8 + bit] |= nthbit(in[dest + 0 * num_parts], bit) << 0 |
                                 nthbit(in[dest + 1 * num_parts], bit) << 1 |
                                 nthbit(in[dest + 2 * num_parts], bit) << 2 |
