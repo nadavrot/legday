57a513845052746d3ac3f8eab293ff372ead5c85
Nadav Rotem <nadav256@gmail.com>
2024-11-25 23:43:55 -0800

    add a transformation that helps to reduce the binary size to 68%

--- lib/liblegday.cpp
+++ lib/liblegday.cpp
@@ -92,7 +92,58 @@ std::optional<bool> legday::BitonicDecoder::decode(uint16_t prob) {
   return bit;
 }
 
+float get_correlation_between_bits(std::span<uint8_t> input, int i, int j) {
+  legday::Stream<16> stream(input);
+  if (i < j) {
+    return 0;
+  }
+  uint32_t correlation = 0;
+  for (size_t k = 0; k < stream.size(); k++) {
+    correlation += stream.get(k, i) == stream.get(k, j);
+  }
+
+  return float(correlation) / float(stream.size());
+}
+
+void print_correlation_matrix(std::span<uint8_t> input) {
+  for (int i = 0; i < 10; i++) {
+    for (int j = 0; j < 10; j++) {
+      printf("(%d, %d) %0.2f ", i, j,
+             get_correlation_between_bits(input, i, j));
+    }
+    printf("\n");
+  }
+}
+
+float get_correlation_between_consecutive_bits(std::span<uint8_t> input,
+                                               int i) {
+  bool prev = false;
+  legday::Stream<16> stream(input);
+
+  uint32_t correlation = 0;
+  for (size_t k = 0; k < stream.size(); k++) {
+    bool bit = stream.get(k, i);
+    correlation += prev == bit;
+    prev = bit;
+  }
+
+  return float(correlation) / float(stream.size());
+}
+
+void print_correlation_previous_bit(std::span<uint8_t> input) {
+  for (int i = 0; i < 16; i++) {
+    printf("%0.2f ", get_correlation_between_consecutive_bits(input, i));
+  }
+  printf("\n");
+}
+
 void add_bias_to_second_byte(std::span<uint8_t> input, uint8_t bias) {
+  // Xor the low exponent bit with the highest mantissa bit.
+  for (size_t i = 0; i < input.size() / 2; i++) {
+    unsigned low_exp = ((input[i * 2 + 1] >> 1) & 0x1);
+    input[i * 2] ^= low_exp << 7;
+  }
+
   for (size_t i = 0; i < input.size() / 2; i++) {
     input[i * 2 + 1] += bias;
   }
@@ -102,23 +153,27 @@ void legday::try_compress(std::span<uint8_t> input) {
   constexpr int CHANNELS = 16;
 
   add_bias_to_second_byte(input, 127);
+  print_correlation_previous_bit(input);
+  print_correlation_matrix(input);
 
   assert(input.size() % CHANNELS == 0);
   std::vector<uint8_t> output;
   Stream<CHANNELS>::ArrayPopcntTy ones;
   Stream<CHANNELS> stream(input);
   stream.popcnt(ones);
-
+  unsigned prev = 0;
   for (int i = 0; i < CHANNELS; i++) {
     BitonicEncoder encoder(output);
     uint16_t prob = uint16_t((ones[i] * 65535) / stream.size());
-    printf("Channel %d: %lu/%zu (%d - %f)\n", i, ones[i], stream.size(), prob,
-           prob / 65535.0);
+
     for (size_t j = 0; j < stream.size(); j++) {
       bool bit = stream.get(j, i);
       encoder.encode(bit, prob);
     }
     encoder.finalize();
+    printf("Channel %d: %lu/%zu (%d - %f) - %lu\n", i, ones[i], stream.size(),
+           prob, prob / 65535.0, output.size() - prev);
+    prev = output.size();
   }
 
   printf("Compressed %zu bytes to %zu bytes\n", input.size(), output.size());
